a129d0d963edde422f1f6d07e074fcb9803525cc
Truong Hao Nam <truonghaonam2003@gmail.com>
2022-12-27 19:31:49 +0700

    bai 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace Project
+{
+    class program
+    {
+        static int GT(int n)
+        {
+            int p = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                p = p * i;
+            }
+            return p;
+        }
+        static int CKN(int k,int n)
+        {
+            int kq = GT(n) / (GT(k) * GT(n - k));
+            return kq;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", CKN(4, 8));
+        }
+    }
+
+}
+
