555dde97db8e70b566766a9442f4d83bfb9391e5
Truong Hao Nam <truonghaonam2003@gmail.com>
2022-12-27 18:39:30 +0700

    h

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-void Xuatmang(int a[], int n);
-void Nhapmang(int a[], int n);
-int main()
-{
-    return 0;
-}
+﻿
